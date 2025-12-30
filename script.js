d403c835d360025e8593f52efcfb22b4b07c1e15
BahaaAbuAlrob <bahaa134579@gmail.com>
2025-12-31 00:22:43 +0200

    Add filter click functionality

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("JavaScript is working!");
+console.log("JS loaded successfully!");
+for (var i = 0; i < filters.length; i++) {
+  filters[i].onclick = function () {
+    for (var j = 0; j < filters.length; j++) {
+      filters[j].classList.remove("active");
+    }
+
+    this.classList.add("active");
+    currentFilter = this.getAttribute("data-filter");
+    drawTasks();
+  };
+}
