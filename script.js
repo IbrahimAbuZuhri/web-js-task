178e29a574b1434d14895285c9efbcf07619dda7
Ibrahim Abu Zuhri <ibrahimabuzuhri@hotmail.com>
2025-12-31 20:13:18 +0200

    add-var's

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("JavaScript is working!");
+var input = document.getElementById("todoInput");
+var addBtn = document.getElementById("addBtn");
+var todoList = document.getElementById("todoList");
+
+var filters = document.getElementsByClassName("filter");
+var deleteDoneBtn = document.getElementById("deleteDone");
+var deleteAllBtn = document.getElementById("deleteAll");
